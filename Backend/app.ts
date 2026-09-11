750bc6a7a8eebf47a25c136c85fe22507abcfa82
YourGitHubUsername <ramuk13476@gmail.com>
2026-09-12 01:07:57 +0530

    setup postgresh and prisma orm

--- Backend/app.ts
+++ Backend/app.ts
@@ -1,12 +1,15 @@
 import express from "express";
 import cors from "cors";
 
+import router from "./routes/auth.route"
+
 
 const app = express();
 app.use
 (cors());
 
 app.use(express.json());
+
 
 app.get("/", ( req:express.Request , res:express.Response ) => {
 
@@ -15,6 +18,7 @@ app.get("/", ( req:express.Request , res:express.Response ) => {
     })
 })
 
+app.use("/api/auth" , router);
 app.listen(3000 , () => {
     console.log( " server listening on port 3000")
 })
--- Backend/controller/authController.ts
+++ Backend/controller/authController.ts
@@ -1,28 +1,31 @@
 import { Request, Response } from 'express';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
+import prisma from '../db/prisma'
 
 
 export const signup = async (req: Request, res: Response) => {
     try {
         const { username, email, password } = req.body;
 
-        const existingUser = await User.findOne({ email });
+        const existingUser = await prisma.user.findOne({ email });
         if (existingUser) {    
             return res.status(400).json({ message: 'User already exists' });
         }
 
         const hashedPassword = await bcrypt.hash(password, 10);
 
-        const newUser = await User.create({
-            username,
-            email,
-            password: hashedPassword
+        const newUser = await prisma.user.create({
+            data: {
+                username,
+                email,
+                password: hashedPassword
+            }
         });
 
         return res.status(201).json({ 
             message: 'User created successfully',
-            userId: newUser._id 
+            userId: newUser.id 
         });
 
     } catch (error) {
@@ -35,7 +38,11 @@ export const login = async (req: Request, res: Response) => {
     try {
         const { email, password } = req.body;
 
-        const user = await User.findOne({ email });
+       const user = await prisma.user.findUnique({
+  where: {
+    email,
+  },
+});
         if (!user) {
             return res.status(400).json({ message: 'Invalid credentials' });
         }
@@ -45,7 +52,7 @@ export const login = async (req: Request, res: Response) => {
             return res.status(400).json({ message: 'Invalid credentials' });
         }
         const token = jwt.sign(
-            { userId: user._id },  process.env.JWT_SECRET || 'secret',  { expiresIn: '1h' }
+            { userId: user.id },  process.env.JWT_SECRET || 'secret',  { expiresIn: '1h' }
         );
 
         return res.status(200).json({ 
