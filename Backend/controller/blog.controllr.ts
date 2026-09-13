a18106093bc01723fb5f2abc6b5de958c13c8277
YourGitHubUsername <ramuk13476@gmail.com>
2026-09-13 13:37:32 +0530

    add the find the single blog

--- Backend/controller/blog.controllr.ts
+++ Backend/controller/blog.controllr.ts
@@ -81,3 +81,44 @@ return res.status(201).json({
         })
     }
  }
+
+
+ export const getBlogById = async ( req: Request , res: Response , next: NextFunction) => {
+
+    try{
+
+        const { id} = req.params
+const blogId = Number(id);
+if(Number.isNaN(blogId)) {
+    return res.status(400).json({
+        success: false,
+        message: " Invalid blog id"
+    })
+}
+
+const blog = await prisma.blog.findUnique({
+    where:{
+        id: blogId
+    }
+})
+
+if( !blog){
+    return res.status(404).json({
+        success: false,
+        message: " Blog not found"
+    })
+}
+
+return res.status(200).json({
+    success: true,
+    message: " Blog fetched successfully",
+    data: blog
+})
+
+    } catch ( error) {
+        return res.status(500).json({
+            success: false,
+            message: " Internal server error "
+        })
+    }
+ }
