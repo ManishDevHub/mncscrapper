be73eb204436eb800a154adbac73243b0e9d454a
ManishDevHub <ramuk13476@gmail.com>
2026-09-14 13:00:40 +0530

    implement auth middleware  on route

--- Backend/routes/blog.route.ts
+++ Backend/routes/blog.route.ts
@@ -1,15 +1,16 @@
 import Router from "express"
 import { createBlog, getAllBlogs, getBlogById, updateBlog,deleteBlog } from "../controller/blog.controllr";
+import { authMiddleware as AuthMiddleware } from "../middleware/auth.middleware";
 
 
 
 const router = Router();
 
-router.post("/create" , createBlog)
-router.get("/", getAllBlogs)
-router.get("/:id", getBlogById)
-router.put("/:id" , updateBlog)
-router.delete("/:id" , deleteBlog)
+router.post("/create" , AuthMiddleware,createBlog)
+router.get("/",AuthMiddleware, getAllBlogs)
+router.get("/:id",AuthMiddleware, getBlogById)
+router.put("/:id" ,AuthMiddleware, updateBlog)
+router.delete("/:id" ,AuthMiddleware, deleteBlog)
 
 
 export default router;
