beba386cab098626d647ef64881e0f41d5d01ef4
ManishDevHub <ramuk13476@gmail.com>
2026-09-17 13:08:07 +0530

    add the update and delete controller

--- Backend/controller/jobController.ts
+++ Backend/controller/jobController.ts
@@ -114,7 +114,7 @@ export const getJobById = async ( req: Request , res: Response , next: NextFunct
             success: true,
             message: "job fetched successfully",
             data: job
-            
+
         })
 
 
@@ -125,3 +125,112 @@ export const getJobById = async ( req: Request , res: Response , next: NextFunct
         })
     }
 }
+
+export const UpdateJob = async ( req: Request , res: Response , next: NextFunction) => {
+
+    try {
+const id = req.params;
+const jobId = Number(id);
+
+if(Number.isNaN(jobId)){
+    return res.status(400).json({
+        success: false,
+        message: 'Invalid job id '
+
+    })
+}
+
+const exitstingJob  = await prisma.job.findUnique({
+
+    where:{
+        id: jobId
+    }
+})
+
+if(!exitstingJob){
+    return res.status(404).json({
+        success: false,
+        message: 'Job not found',
+
+    })
+}
+
+ const { title, company, location,jobType, experience, description, skills, salary, applyUrl,isActive, } = req.body;
+
+
+ const updatedJob = await prisma.job.update({
+    where:{
+        id: jobId
+    },
+    data:{
+         title,
+        company,
+        location,
+        jobType,
+        experience,
+        description,
+        skills,
+        salary,
+        applyUrl,
+        isActive,
+
+    }
+ })
+
+ return res.status(200).json({
+    success: true,
+    message: "Job updated successfully",
+    data: updatedJob,
+
+ })
+    } catch (error) {
+        return res.status(500).json({
+            success:false,
+            message: 'Internal server error'
+        })
+    }
+}
+
+export const deleteJob = async ( req: Request ,res: Response, next: NextFunction) => {
+  try {
+    const { id } = req.params;
+
+    const jobId = Number(id);
+
+    if (Number.isNaN(jobId)) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid job ID",
+      });
+    }
+
+    const existingJob = await prisma.job.findUnique({
+      where: {
+        id: jobId,
+      },
+    });
+
+    if (!existingJob) {
+      return res.status(404).json({
+        success: false,
+        message: "Job not found",
+      });
+    }
+
+    await prisma.job.delete({
+      where: {
+        id: jobId,
+      },
+    });
+
+    return res.status(200).json({
+      success: true,
+      message: "Job deleted successfully",
+    });
+  } catch (error) {
+    return res.status(500).json({
+        success:false,
+        message: 'Internal server error'
+    })
+  }
+};
