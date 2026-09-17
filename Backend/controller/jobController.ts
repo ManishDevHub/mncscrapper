1ee42dca4bed1d0f0f6a08db37f3d41a845b8974
ManishDevHub <ramuk13476@gmail.com>
2026-09-17 12:34:22 +0530

    create job controller

---
+++ Backend/controller/jobController.ts
@@ -0,0 +1,50 @@
+import { Request , Response, , NextFunction } from 'express'
+import prisma from '../db/prisma'
+
+
+export const createJob = async ( req: Request , res: Response , next: NextFunction) => {
+
+
+
+    try {
+        const { title , company , location , jobType , exprerience , skills , 
+            salary , applyLink 
+        } = req.body
+
+        if(!title || !company || !location || !jobType || !exprerience || !skills || !salary || !applyLink){
+            return res.status(400).json({
+                success: false,
+                message: 'All fields are required'
+            })
+        }
+
+        const job = await prisma.job.create({
+
+            data: {
+                title,
+                company,
+                location,
+                jobType,
+                exprerience,
+                skills,
+                salary,
+                applyLink
+            }
+        })
+        return res.status(201).json({
+            success: true , 
+            message: 'Job created successfully',
+            data: job
+            
+
+        })
+
+
+    } catch ( error) {
+        return res.status(500).json({
+            success: false,
+            message: 'Internal server error'
+        })
+
+    }
+} 
